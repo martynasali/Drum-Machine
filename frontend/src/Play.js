b85c36157a746439b1e662b55168bf3bedfe5df9
martynasali <alijosius.martynas@gmail.com>
2021-11-30 23:34:16 +0200

    tyding

--- frontend/src/Play.js
+++ frontend/src/Play.js
@@ -5,6 +5,7 @@ export default function Play () {
 
     const synth = new Tone.Synth().toDestination();
     const now = Tone.now()
+    const song = []
     
     const icon = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" /><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
     const playStyle ='justify-items-center h-8 w-8 bg-gray-500'
@@ -13,6 +14,13 @@ export default function Play () {
     
     function start (){
         if (play === playStyle ){
+
+            const phaser = new Tone.Phaser({
+	frequency: 15,
+	octaves: 5,
+	baseFrequency: 1000
+}).toDestination();
+
            const synth = new Tone.MembraneSynth({
   pitchDecay:0.05,
   octaves: 4,
@@ -31,11 +39,14 @@ export default function Play () {
     attackCurve :"exponential"
   }
 }).toDestination();
+
 const seq = new Tone.Sequence((time, note) => {
-	synth.triggerAttackRelease(note, 0.1, time);
+	synth.triggerAttackRelease(note, 0.3, time);
 	// subdivisions are given as subarrays
-}, ["C4", ["E4", "D4", "E4"], "G4", ["A4", "G4"]]).start(0);
+}, ["D4", "A4", "A4","F4","f4","f4","f4","f4","F4","f4","D4","A4","A4","A4","f4" ]).start(0);
+Tone.Transport.bpm.value = 80;
 Tone.Transport.start();
+
         }else{
             setPlay(playStyle)
 
--- frontend/src/SoundButton.js
+++ frontend/src/SoundButton.js
@@ -1,59 +1,166 @@
 import { useState } from "react";
 import React  from 'react';
+import * as Tone from 'tone'
+
 
 const buttonClass = " flex-1 h-8 p-6 w-8 rounded-lg bg-gray-700 border-4 border-border-gray-500 border-opacity-100 hover:border-pink-400 hover:bg-gray-800";
 const buttonClassFour = " flex-1 h-8 p-6 w-8 rounded-lg bg-gray-500 border-4 border-border-gray-500 border-opacity-100 hover:border-pink-400 hover:bg-gray-800";
 const buttonClassSlected = " flex-1 h-8 p-6 w-8 rounded-lg bg-yellow-400 border-4 border-border-gray-500 border-opacity-100 hover:border-pink-400";
-const song = []
-let index = 0
+
+
+function NewButton ()
+{
+    
+    
+    const buttons = []
+    for (var i = 0; i < 16; i++) {
+        if((i+1) %4 === 0){
+            
+            buttons.push({
+                index: i,
+                isOn: null,
+                style: buttonClassFour
+        })}
+        else{
+    buttons.push({
+        index: i,
+        isOn: null,
+        style: buttonClass
+    })}
+    console.log(buttons);
+    }
+    
+    const [button, setButton] = useState(buttons)
+    
+    function TurnOn ({indexas=7}) 
+    {
+        const butts = button
+        console.log(butts);
+        console.log(indexas);
+
+    // if((indexas+1) % 4 == 0){
+    //     butts[indexas].style = buttonClassSlected
+    //     butts[indexas].isOn = true
+    //     setButton(butts)
+    //     console.log(indexas);
+    // }
+    // else{
+        butts[indexas].style = buttonClassSlected
+        butts[indexas].isOn = true   
+        setButton(butts)
+        // }
+        
+}
+
+return  button.map( b =>
+   <div indexas={b.index} className={b.style} onClick={TurnOn} >{b.index}</div>  
+)
+}
+
+
+
 function Button ({number})
 {
+    
     const [btnClass, setBtnClass] = useState(buttonClass)
-
+    
     function setClass () {
-    if (btnClass == buttonClass || btnClass == buttonClassFour){
-        setBtnClass(buttonClassSlected)
-    }else{
-        setBtnClass(buttonClass)
+        if (btnClass == buttonClass || btnClass == buttonClassFour){
+            setBtnClass(buttonClassSlected)
+            
+        }else{
+            setBtnClass(buttonClass)
+        }
     }
-  }
     return <div class={btnClass} onClick={setClass}>{number}</div> 
 }
+
+
+export default function SoundButton (){
+    let nwsong = [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null ]
+    const [song, setSong] = useState(nwsong)
 
 function ButtonFour ({number})
 {
     const [btnClass, setBtnClass] = useState(buttonClassFour)
+    nwsong = song
     function setClass () {
     if (btnClass == buttonClass || btnClass == buttonClassFour){
         setBtnClass(buttonClassSlected)
+        nwsong[number] = "C4"
+        console.log(nwsong, nwsong[number]);
+        setSong(nwsong)
     }else{
         setBtnClass(buttonClassFour)
-    }
+        nwsong[number] = null
+        console.log(nwsong, nwsong[number]);
+        setSong(nwsong)
+  }}
+
+function plau (){
+// -------------------------------      
+const phaser = new Tone.Phaser({
+	frequency: 15,
+	octaves: 5,
+	baseFrequency: 1000
+}).toDestination();
+
+           const synth = new Tone.MembraneSynth({
+  pitchDecay:0.05,
+  octaves: 4,
+  oscillator : {
+    type :"fmsine",
+    phase: 140,
+    modulationType: "sine",
+    modulationIndex:0.8,
+    partials: [1] //1,0.1,0.01,0.01
+  },
+  envelope :{
+    attack:0.01,
+    decay :0.74,
+    sustain: 0.71,
+    release: 0.05,
+    attackCurve :"exponential"
   }
-    return <div class={btnClass} onClick={setClass}>{number}</div> 
+}).toDestination();
+
+const seq = new Tone.Sequence((time, note) => {
+	synth.triggerAttackRelease(note, 0.3, time);
+	// subdivisions are given as subarrays
+}, song).start(0);
+Tone.Transport.bpm.value = 180;
+Tone.Transport.start();
+    // -------------------
+  }
+  
+
+return <> <div class={btnClass} onClick={setClass}>{number} Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit eius facere impedit, placeat illum aspernatur tempora consequatur animi ipsa dicta hic, numquam in? Facilis repellat cumque, veniam corrupti praesentium dolores.  </div>
+<button onClick={plau}>Plau</button> 
+</>
 }
 
-export default function SoundButton (){
     return(
         
         <>
         <div className="flex flex-row"> 
+        {/* <Button number={0}/>
         <Button number={1}/>
         <Button number={2}/>
-        <Button number={3}/>
-        <ButtonFour number={4}/>
+        <ButtonFour number={3}/>
+        <Button number={4}/>
         <Button number={5}/>
         <Button number={6}/>
-        <Button number={7}/>
-        <ButtonFour number={8}/>
+        <ButtonFour number={7}/>
+        <Button number={8}/>
         <Button number={9}/>
         <Button number={10}/>
-        <Button number={11}/>
-        <ButtonFour number={12}/>
+        <ButtonFour number={11}/>
+        <Button number={12}/>
         <Button number={13}/>
         <Button number={14}/>
-        <Button number={15}/>
-        <ButtonFour number={16}/>
+        <ButtonFour number={15}/> */}
+        
+        <NewButton/>
         </div>    
         </>
         )
