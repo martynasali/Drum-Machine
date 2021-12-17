8d3dba6cca5095391358898072a394fd92981afc
martynasali <alijosius.martynas@gmail.com>
2021-12-17 14:27:48 +0200

    tidy

--- frontend/src/Player.js
+++ frontend/src/Player.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Buttons from "./Buttons";
+// import Buttons from "./Buttons";
 import { useState, useEffect } from "react";
 import * as Tone from 'tone'
 let seq
@@ -35,27 +35,35 @@ let playing = false
   }
 }
 
+const buttonClass = "h-8 p-6 w-8 rounded-lg bg-gray-700 border-4 border-border-gray-500 border-opacity-100 hover:border-pink-400 hover:bg-gray-800";
+const buttonClassSelect = " h-8 p-6 w-8 rounded-lg bg-yellow-400 border-4 border-border-gray-500 border-opacity-100 hover:border-pink-400";
+const buttonClassFour = "h-8 p-6 w-8 rounded-lg bg-gray-500 border-4 border-border-gray-500 border-opacity-100 hover:border-pink-400 hover:bg-gray-800";
+
 export default function Player ()
 {
+
+
 const [playing1, setPlaying1]= useState(false)
 
-    const buttons = []
+    const buttonsE = []
     for (let i = 0; i < 16; i++) {
         if((i+1) %4 === 0){
 
-            buttons.push({
+            buttonsE.push({
                 index: i,
-                isOn: null
+                isOn: null,
+                style: "h-8 p-6 w-8 rounded-lg bg-gray-500 border-4 border-border-gray-500 border-opacity-100 hover:border-pink-400 hover:bg-gray-800"
         })}
         else{
-    buttons.push({
+    buttonsE.push({
         index: i,
-        isOn: null
+        isOn: null,
+        style: "h-8 p-6 w-8 rounded-lg bg-gray-700 border-4 border-border-gray-500 border-opacity-100 hover:border-pink-400 hover:bg-gray-800"
     })}
     }
-    const [buttons_i, setButtons_i] = useState( buttons.map(i=>{return{...i}}))
-    const [buttons_j, setButtons_j] = useState(buttons.map(j=>{return{...j}}))
-    const [buttons_k, setButtons_k] = useState(buttons.map(k=>{return{...k}}))
+    const [buttons_i, setButtons_i] = useState(buttonsE.map(i=>{return{...i}}))
+    const [buttons_j, setButtons_j] = useState(buttonsE.map(j=>{return{...j}}))
+    const [buttons_k, setButtons_k] = useState(buttonsE.map(k=>{return{...k}}))
 
 
 function initialize(){
@@ -77,9 +85,9 @@ function initialize(){
         synths_k.triggerAttackRelease(note, 0.3, time);
     }, song_k).start(0);
 
-    let timing = new Tone.Sequence((time, note) => {
-        console.log(note)
-    }, ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16"]).start(0);
+    // let timing = new Tone.Sequence((time, note) => {
+    //     console.log(note)
+    // }, ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16"]).start(0);
 
 
 }
@@ -91,23 +99,25 @@ console.log("hi");
 // oscillator(synth, song)
 //   let song = ["D4", "A4", "A4","F4","f4","f4","f4","f4","F4","f4","D4","A4","A4","A4","f4" ]
 function change(){
+    ini = false
+    playing = false
+    // seq.stop();
+    // seq_j.stop();
+    // seq_k.stop();
+
     song_i = buttons_i.map(b=>b.isOn)
     song_j = buttons_j.map(b=>b.isOn)
     song_k = buttons_k.map(b=>b.isOn)
-    console.table( buttons_i);
-    console.table( buttons_j);
-    console.table( buttons_k);
-    ini = false
-    playing = false
-    seq.stop();
-    seq_j.stop();
-    seq_k.stop();
+    console.table("song_i", song_i)
+    console.table("song_j", song_j)
+    console.table("song_k", song_k)
+
     if(playing === false){
-        initialize()
+        // initialize()
         Tone.start();
-        seq.start(0);
-        seq_j.start(0);
-        seq_k.start(0);
+        // seq.start(0);
+        // seq_j.start(0);
+        // seq_k.start(0);
         playing = true
     }
     else
@@ -117,35 +127,89 @@ function change(){
     }
 }
 useEffect(()=>{
-    initialize()
+    // initialize()
 })
 
     function play () {
+    synths = new Tone.MembraneSynth(synth).toDestination();
+    let synthsw = new Tone.MembraneSynth(synth).toDestination();
+    let synthsa = new Tone.MembraneSynth(synth).toDestination();
+    seq = new Tone.Sequence((time, note) => {
+        synthsw.triggerAttackRelease(note, 0.3, time);
+    }, song_i).start(0);
+    let seqr = new Tone.Sequence((time, note) => {
+        synthsa.triggerAttackRelease(note, 0.3, time);
+    }, song_j).start(0);
+    let seqt = new Tone.Sequence((time, note) => {
+        synths.triggerAttackRelease(note, 0.3, time);
+    }, song_k).start(0);
         Tone.Transport.bpm.value = 120; //how many beats(quarter notes) per minute
-        if(!playing1){
         Tone.Transport.start();
-        ini=true;
-        setPlaying1(true);
-        }else{
-           Tone.Transport.stop();
-           ini=false
-           setPlaying1(false)
-           seq.stop();
-           seq_j.stop();
-           seq_k.stop();
+
+    }
+    useEffect(() => {
+        change()
+    },[buttons_i, buttons_j, buttons_k])
+
+
+    function Buttons ({buttons, sound, setButton})
+    {
+
+        function Button ({number, sound, className})
+        {
+            function setStyle() {
+
+                if(buttons[number].isOn === null)
+                {
+                    // jeigu paprastas tampa geltonu
+                    let buttonsCopy = buttons.map(i=>{return{...i}})
+                    buttonsCopy[number].isOn = sound
+                    buttonsCopy[number].style = buttonClassSelect
+                    setButton(buttonsCopy.map(bc=>{return{...bc}}))
+                }
+                else if((buttons[number].index+1) % 4 === 0)
+                {
+                    // jeigu ketvirtasis, grįžta į pilką
+                    let buttonsCopy = buttons.map(i=>{return{...i}})
+                    buttonsCopy[number].isOn = null;
+                    buttonsCopy[number].style = buttonClassFour;
+                    setButton(buttonsCopy.map(bc=>{return{...bc}}))
+                }
+                else
+                {
+                    // jeigu ne ketvirtasis, grįžta į tamsų
+                    let buttonsCopy = buttons.map(i=>{return{...i}})
+                    buttonsCopy[number].isOn = null;
+                    buttonsCopy[number].style = buttonClass;
+                    setButton(buttonsCopy.map(bc=>{return{...bc}}))
+
+                }
+
+            }
+
+            return <div onClick={setStyle} className={className}></div>
+
         }
 
+
+        return (
+            <div className="flex justify-center">
+                {buttons.map(b=><>
+                    { <Button className={b.style}  sound={sound} number={b.index}/> }
+                </>)}
+            </div>
+        )
     }
 
 
-return<> 
+
+    return<>
 {/*<button className="w-16 h-16 rounded-lg border-4 border-border-gray-500 border-opacity-100 hover:border-pink-400 hover:bg-red-900 bg-purple-500 " onClick={change}>Stop</button>*/}
 <button className="w-16 h-16 rounded-lg border-4 border-border-gray-500 border-opacity-100 hover:border-pink-400 hover:bg-red-900 bg-purple-500 " onClick={play}>{playing1?"Stop":"Play"}</button>
-<div onClick={change}>
-<Buttons sound={"C1"} setbutton={setButtons_i}  buttons={buttons_i}/>
-<Buttons sound={"C2"} setbutton={setButtons_j} buttons={buttons_j}/>
-<Buttons sound={"C3"} setbutton={setButtons_k} buttons={buttons_k}/>
-</div>
+
+<Buttons sound={"C1"} setButton={setButtons_i} buttons={buttons_i}/> hi
+<Buttons sound={"C2"} setButton={setButtons_j} buttons={buttons_j}/>
+<Buttons sound={"C3"} setButton={setButtons_k} buttons={buttons_k}/>
 </>
 
 }
