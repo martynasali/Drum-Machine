fa13bac26f54ffad562e4e6bf93c237f240f65a5
martynasali <alijosius.martynas@gmail.com>
2021-12-26 22:04:19 +0200

    fin

---
+++ project/src/App.js
@@ -0,0 +1,24 @@
+import Player from './Player';
+import './App.css';
+import './index.css';
+
+
+
+
+function App() {
+  
+
+
+    return (
+<>
+    <div className="h-48"></div>
+    
+    <Player />
+  
+    
+</> 
+)
+
+}
+
+export default App;
